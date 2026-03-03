d68cbff3a1094640842a5ed9f5eeb7aa4ee76fbc
diegocmn <diegocmn@gmail.com>
2026-03-02 23:37:12 -0300

    creation of project

--- README.md
+++
@@ -1,2 +0,0 @@
-# estudoJavaDEV
-Estudos java  voltados a programação web  e backend
---
+++ javaDev/src/main/java/br/com/javadev/JavaDEV.java
@@ -0,0 +1,10 @@
+package br.com.javadev;
+
+public class JavaDEV {
+
+	public static void main(String[] args) {
+		System.out.println("ola");
+
+	}
+
+}
